04d881d2e3455209be349c1a348f2adc348c0470
Alaminos <aalaminosz@gmail.com>
2018-10-12 22:00:14 +0200

    hi

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const data = {
-  layers: ["application", "presentation", "session", "transport"],
+  layers: [
+    "application",
+    "presentation",
+    "session",
+    "transport",
+    "network",
+    "data link",
+    "physical"
+  ],
 
-  Protocoles: ["fjaksl, tsldm...", "gls, gsg...", "gls, gsg..."],
+  Protocoles: [
+    "HTTP, FTP, POP3...",
+    "ASCII, MPEG, SSL...",
+    "NetBIOS, Handshaking connection...",
+    "TCP, UDP",
+    "IPv4, ARP...",
+    "Ethernet, MAC Addresses,...",
+    "Cables, Hubs..."
+  ],
+
+  Datagrammes: ["Data", "Data", "Data", "Segment", "Packet", "Frame", "Bits"],
 
   Description: []
 };
